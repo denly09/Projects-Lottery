eaa208d31dcfeb3062b83ab5125d9879c9442934
Denly09 <denniely.garcia09@gmail.com>
2022-12-01 17:59:09 +0800

    [feature 4 ]edit registration admin and client

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params
+  before_action :configure_account_update_params, only: [:update, :edit]
 
   # GET /resource/sign_up
   def new
@@ -10,19 +10,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
   # def destroy
@@ -40,15 +40,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    added_attrs = [:username, :email, :phone_number, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+  end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_account_update_params
+    change = [:username, :email, :phone_number, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit(:account_update, keys: change)
+  end
+
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
