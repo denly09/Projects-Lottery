cd69469a22738786b398cebdc72e29f6e1b8ca92
Denly09 <denniely.garcia09@gmail.com>
2022-12-06 19:05:55 +0800

    [feature 7] add cookie for sign_up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,27 +1,38 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params
-  before_action :configure_account_update_params, only: [:update, :edit]
+  before_action :configure_sign_up_params, only:  [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
     super
+    unless cookies[:promoter]
+      cookies[:promoter] = params[:promoter]
+    end
   end
 
   # POST /resource
   def create
+    params[:user][:parent_id] = User.find_by_email(cookies[:promoter])&.id
     super
   end
 
   # GET /resource/edit
   def edit
     super
+    # flash[:notice]
   end
 
   # PUT /resource
   def update
-    super
+    parameter = params[:user]
+    if parameter[:current_password].present? || parameter[:password_confirmation].present? || parameter[:password].present?
+      update_with_pass
+      # flash[:notice] = ''
+    else
+      update_without_pass
+    end
   end
 
   # DELETE /resource
@@ -38,18 +49,41 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+  def params_with_pass
+    params.require(:user).permit(:current_password, :password_confirmation, :password)
+  end
 
+  def params_without_pass
+    params.require(:user).permit(:username, :phone_number, :image, :current_password, :password_confirmation, :password)
+  end
+
+  def update_without_pass
+    if params[:user][:current_password].blank?
+      resource.update_without_password(params_without_pass.except(:current_password))
+      redirect_to me_path
+    else
+      render :edit
+    end
+  end
+
+  def update_with_pass
+    if resource.update_with_password(params_with_pass)
+      redirect_to new_user_session_path
+    else
+      render :edit
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    added_attrs = [:username, :email, :phone_number, :password, :password_confirmation, :remember_me]
+    added_attrs = [:parent, :email, :phone_number, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    change = [:username, :email, :phone_number, :password, :password_confirmation, :remember_me]
+    change = [:email, :phone_number, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit(:account_update, keys: change)
   end
 
