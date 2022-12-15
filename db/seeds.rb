64bc7eacf61582d68efb3e3dac765ac0d111b26f
Denly09 <denniely.garcia09@gmail.com>
2022-12-15 16:23:25 +0800

    [Feature 12]Add categories in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Category.find_or_create_by(name: 'Phone')
+Category.find_or_create_by(name: 'Tv')
+Category.find_or_create_by(name: 'Perfume')
+Category.find_or_create_by(name: 'Electronics')
